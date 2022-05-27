2739b2f1ef9a3319911f40b6211d051d194280b5
jürivaitmaa <jyrivaitmaa@gmail.com>
2022-05-27 17:28:31 +0300

    27-05 angualr tund

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListEmployeesComponent } from './employees/list-employees/list-employees.component';
+import { ListEmployeesComponent } from './employees/list-employees.component';
+import { CreateEmployeeComponent } from './employees/create-employee.component';
+import { FormsModule }   from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListEmployeesComponent
+    ListEmployeesComponent,
+    CreateEmployeeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
--- src/app/models/employee.models.ts
+++ src/app/models/employee.models.ts
@@ -4,7 +4,7 @@ export class Employee{
     gender: string;
     email?: string;
     phoneNumber?: number;
-    contactPreferance: string;
+    contactPreference: string;
     dateOfBirth: Date;
     department: string;
     isActive: boolean;
